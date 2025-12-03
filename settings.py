fbd72e5ccf29a5ce4bf9775f224197405942c801
bahyani <ikennaiwu@yahoo.com>
2025-12-03 06:35:17 +0100

    GraphQL

---
+++ settings.py
@@ -0,0 +1,6 @@
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
